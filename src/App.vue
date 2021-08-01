a0ae44899f381ddc1b65024185dfa5b54477b948
ParkYoungWoong <thesecon@gmail.com>
2021-08-01 17:06:50 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <hello />
+    <router-view />
+    <div></div>
+  </div>
+</template>
+
+<script>
+import Hello from '@/components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
